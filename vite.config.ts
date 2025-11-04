280a7bfcce4879c69b948f03163a0ce33dd687ad
Luiz Pedro Galdino Silva <luispedrogaldino53@gmail.com>
2025-11-04 13:01:07 -0300

    Configurado caminho do servidor express criado

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Proxy para facilitar desenvolvimento: encaminha /api para o servidor Express local (porta 3001)
+  // Proxy para facilitar desenvolvimento: encaminha /api para o servidor Express https://servidor-igreja-site.onrender.com/
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://servidor-igreja-site.onrender.com',
         changeOrigin: true,
         secure: false,
       },
